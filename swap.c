212874a2c3eff14d43ced5d89b8317e3c9100069
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2024-09-30 15:24:39 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    c=a+b;
+    a=b;
+    b=c;
+    c=a;
+    printf("%d",a);
+    printf("%d",b);
+    printf("%d",c);
+    return 0;
+}
