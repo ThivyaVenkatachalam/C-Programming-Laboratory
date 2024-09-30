4a89e82f35ab00d4a157bb19eb8e72973a6c344c
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2024-09-30 20:35:45 +0530

    Update compoundinterest.c

--- compoundinterest.c
+++ compoundinterest.c
@@ -5,7 +5,7 @@ int main()
     float p,r,t;
     float cp,am;
     scanf("%f %f %f",&p,&r,&t);
-    am= p*((pow((1 + r/ 100),t)));
+    am= p*(pow(1 + r/ 100),t));
     printf("%f",am);
     cp=am-p;
     printf("%f",cp);
