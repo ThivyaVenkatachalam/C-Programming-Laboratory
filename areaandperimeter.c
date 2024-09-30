66ce7c8903851672a41a3e1d841ae4a904f38113
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2024-09-30 15:45:08 +0530

    Create areaandperimeter.c

---
+++ areaandperimeter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,l,b,p;
+    scanf("%d",&l);
+    scanf("%d",&b);
+    a=l*b;
+    printf("Area:%d",a);
+    p=(2*(l+b));
+    printf("perimeter:%d",p);
+    return 0;
+}
