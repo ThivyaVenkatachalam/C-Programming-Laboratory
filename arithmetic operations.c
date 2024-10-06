c6c4572c62a689113d34fb05fb860cd6a8dd56c9
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2024-10-06 16:56:47 +0530

    Create arithmetic operations.c

---
+++ arithmetic operations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    printf("enter two integer: ");
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    return 0;
+}
