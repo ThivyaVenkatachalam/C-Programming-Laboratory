7610780f70bbad8dea2327f4a6b516ba48ad415e
ThivyaVenkatachalam <thivyavenkat15@gmail.com>
2024-10-06 17:56:30 +0530

    Create dodecahedron.c

---
+++ dodecahedron.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float v,e;
+    printf("side:");
+    scanf("%f",&e);
+    v=(15+7*sqrt(5))/4*pow(e,3);
+    printf("%.2f",v);
+    return 0;
+    
+}
